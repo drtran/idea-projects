b883e351fa7de266b12a66ab6a156fc3016b5a78
Kiet Tran <test@test.com>
2017-11-26 17:27:19 +0000

    Initial commit

---
+++ QuickStartDemo/README.md
@@ -0,0 +1,5 @@
+# Introduction
+This is a sample markdown.
+
+1. greetings 1
+1. greetings 2
---
+++ QuickStartDemo/src/com/drkiettran/Main.java
@@ -0,0 +1,8 @@
+package com.drkiettran;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Greeings from Herndon VA!");
+    }
+}
